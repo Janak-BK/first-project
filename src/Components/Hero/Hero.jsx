c8eb24fcd630160c3471c604e4003b10063e44e0
Janak-BK <subinsunar000@gmail.com>
2024-04-16 22:56:35 +0545

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Hero.css";
+import play from "../../assets/play_icon.png";
+import arrow from "../../assets/arrow_btn.png";
+import pause from "../../assets/pause_icon.png"
+
+const Hero = ({
+  heroData,
+  setHeroCount,
+  heroCount,
+  setPlayStatus,
+  playStatus,
+}) => {
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <p>{heroData.Text1}</p>
+        <p>{heroData.Text2}</p>
+      </div>
+      <div className="hero-explore">
+        <p>Explore the feature</p>
+        <img src={arrow} alt="" />
+      </div>
+      <div className="hero-dot-play">
+        <ul className="hero-dots">
+          <li
+            onClick={() => setHeroCount(0)}
+            className={heroCount === 0 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(1)}
+            className={heroCount === 1 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(2)}
+            className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+        </ul>
+        <div className="hero-play">
+          <img onClick={()=>setPlayStatus(!playStatus)} src={playStatus?pause:play} alt="" />
+          <p>See the video</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
